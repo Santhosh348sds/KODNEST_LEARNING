a2c9270f03a65f3da20a1b99c24a81d358d7fd6f
Santhosh S <santhoshsanthosh86920@gmail.com>
2023-12-10 16:59:28 +0530

    Concepts
    This Branch contains all Java Concepts

--- CharacterIdentifier/src/com/kodnest/practice/CharacterIdentifier/Main.java
+++
@@ -1,9 +0,0 @@
-package com.kodnest.practice.CharacterIdentifier;
-
-public class Main {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- Labeled_Loop/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Labeled_Loop {
-}
